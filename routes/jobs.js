6b2b776a8df372c9f973b5adfb393fe51437d66f
John Alfano <alf027@gmail.com>
2015-09-14 10:35:50 -0600

    new and index

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,11 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('jobs/index', { title: 'Jobs' });
+  var db = req.db;
+  var jobs = db.get('jobs');
+
+  jobs.find({},function(err,docs){
+    res.render('jobs/index', { title: 'Jobs' , jobs:docs});
+  })
+
+
 });
 
 router.get('/new',function(req,res,next){
   res.render('jobs/new',{})
+});
+
+router.post('/',function(req,res,next){
+  var db = req.db;
+  var jobs = db.get('jobs');
+  var posting = req.body;
+  posting.isOpen = true;
+  posting.date = new Date();
+
+  console.log(posting.date.toJSON().slice(0,10));
+  console.log(posting);
+  jobs.insert(posting,function(err,doc){
+    if(err){
+      console.log(err)
+      res.redirect('/jobs/new')
+    } else {
+      res.redirect('/jobs')
+    }
+  })
+
+
 });
 
 module.exports = router;
